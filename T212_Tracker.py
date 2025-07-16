9e36eefdb4315d4f2a201c4e2f629739ea50d750
mace12345 <samuelmace98@gmail.com>
2025-07-16 19:04:46 +0100

    update

--- T212_Tracker.py
+++ T212_Tracker.py
@@ -3,6 +3,8 @@ import pandas as pd
 import time
 import os
 import numpy as np
+import yfinance as yf
+
 
 class T212Portfolio:
     def __init__(
@@ -10,13 +12,125 @@ class T212Portfolio:
         portfolio_path: str,
         T212_API_KEY: str,
         AV_API_KEY: str,
-        T212_URL = "https://live.trading212.com/api/v0/equity/portfolio",
-        AV_BASE_URL = "https://www.alphavantage.co/query",
+        FINNHUB_API_KEY: str,
+        T212_URL="https://live.trading212.com/api/v0/equity/portfolio",
+        AV_BASE_URL="https://www.alphavantage.co/query",
     ):
         self.portfolio_path = portfolio_path
         self.T212_API_KEY = T212_API_KEY
         self.AV_API_KEY = AV_API_KEY
-        self.T212
+        self.FINNHUB_API_KEY = (FINNHUB_API_KEY,)
+        self.T212_URL = T212_URL
+        self.AV_BASE_URL = AV_BASE_URL
+
+        self.T212_Ticker_df = pd.read_csv(
+            os.path.join(os.getcwd(), "T212_Ticker_Dict.csv"),
+        )
+        self.T212_Ticker_df.set_index("Trading 212 Ticker", inplace=True)
+        self.T212_Ticker_df.fillna("NO_DATA", inplace=True)
+
+        self.PortfolioDF = pd.DataFrame()
+        self.fx_cache = {
+            "GBP_to_USD": self.get_exchange_rate("GBP", "USD"),
+            "USD_to_GBP": self.get_exchange_rate("USD", "GBP"),
+            "EUR_to_GBP": self.get_exchange_rate("EUR", "GBP"),
+            "GBP_to_EUR": self.get_exchange_rate("GBP", "EUR"),
+            "CAD_to_GBP": self.get_exchange_rate("CAD", "GBP"),
+            "GBP_to_CAD": self.get_exchange_rate("GBP", "CAD"),
+            "GBP_to_GBX": 100,
+            "GBX_to_GBP": 0.01,
+            "GBP_to_GBP": 1,
+        }
+
+    def get_exchange_rate(self, base, target):
+        url = f"https://api.frankfurter.app/latest?from={base}&to={target}"
+        response = requests.get(url)
+        data = response.json()
+        rate = data["rates"][target]
+        return rate
+
+    def get_company_profile(self, ticker, api_key):
+        url = f"https://finnhub.io/api/v1/stock/profile2"
+        params = {"symbol": ticker, "token": api_key}
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        # Extract desired fields
+        name = data.get("name", "N/A")
+        country = data.get("country", "N/A")
+        industry = data.get("finnhubIndustry", "N/A")
+        sector = data.get("sector", "N/A")  # Only available for some stocks
+
+        return {
+            "name": name,
+            "country": country,
+            "industry": industry,
+            "sector": sector,
+        }
+
+    def update_sectors_industries_T212_Ticker_df(self):
+        for idx, row in self.T212_Ticker_df.iterrows():
+            ticker = row["Actural Ticker"]
+            if any(
+                [
+                    row["Country"] == "NO_DATA",
+                    row["Country"] == "NO_DATA",
+                    row["Country"] == "NO_DATA",
+                ]
+            ):
+                data = self.get_company_profile(
+                    ticker=ticker, api_key=self.FINNHUB_API_KEY
+                )
+                if data["country"] != "N/A":
+                    self.T212_Ticker_df.loc[idx, "Country"] = data["country"]
+                if data["sector"] != "N/A":
+                    self.T212_Ticker_df.loc[idx, "Sector"] = data["sector"]
+                if data["industry"] != "N/A":
+                    self.T212_Ticker_df.loc[idx, "Industry"] = data["industry"]
+            self.T212_Ticker_df.to_csv(
+                os.path.join(os.getcwd(), "T212_Ticker_Dict.csv")
+            )
+            time.sleep(1)
+
+    def fetch_portfolio(self):
+        headers = {"Authorization": self.T212_API_KEY}
+        try:
+            res = requests.get(self.T212_URL, headers=headers)
+            res.raise_for_status()
+            data = res.json()
+        except requests.RequestException as e:
+            print(f"Error fetching portfolio: {e}")
+            return None
+        for idx, share in enumerate(data):
+
+            t212_ticker = share["ticker"]
+            ticker = self.T212_Ticker_df.loc[t212_ticker, "Actural Ticker"]
+            name = self.T212_Ticker_df.loc[t212_ticker, "Instrament Name"]
+            currency = self.T212_Ticker_df.loc[t212_ticker, "Currency"]
+
+            quantity = share["quantity"]
+            current_price = share["currentPrice"]
+            position_value = quantity * current_price
+            position_value_GBP = position_value * self.fx_cache[f"{currency}_to_GBP"]
+
+            self.PortfolioDF.loc[idx, "Ticker"] = ticker
+            self.PortfolioDF.loc[idx, "Instrament Name"] = name
+            self.PortfolioDF.loc[idx, "Currency"] = currency
+            self.PortfolioDF.loc[idx, "Quantity"] = quantity
+            self.PortfolioDF.loc[idx, "Current Price"] = current_price
+            self.PortfolioDF.loc[idx, "Position Value"] = round(position_value, 2)
+            self.PortfolioDF.loc[idx, "Position Value (GBP)"] = round(
+                position_value_GBP, 2
+            )
+
+        self.PortfolioDF.to_csv(self.portfolio_path, index=False)
+
 
 if "__main__" == __name__:
-    print("Hi")
+    t212 = T212Portfolio(
+        portfolio_path=os.path.join(os.getcwd(), "T212_Portfolio.csv"),
+        T212_API_KEY="33802252ZVGbljRTOZojjmDWqtOEfAgKdUMCZ",
+        AV_API_KEY="89PV2157O3JISFQ3",
+        FINNHUB_API_KEY="d1rtj39r01qskg7q99bgd1rtj39r01qskg7q99c0",
+    )
+    # t212.fetch_portfolio()
